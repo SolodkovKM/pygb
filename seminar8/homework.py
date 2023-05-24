efd66a26bd516f785e6415a13b161993e63d8756
kost <solodkovkm1997@gmail.com>
2023-05-24 15:06:47 +0300

    Seminar8/homework edit

--- seminar8/homework.py
+++ seminar8/homework.py
@@ -1,46 +1,52 @@
+def read_file():
+    with open('base.txt', 'r+', encoding="utf-8") as file:
+        return file.read()
+
+def write_file(string):
+    with open('base.txt', 'a', encoding="utf-8") as file:
+        file.write(string + "\n")
+
 def ShowBase():
-    with open('base.txt', 'r', encoding="utf-8") as file:
-        print("_________________________________________________")
-        while True:
-            user = file.readline()
-            if not user:
-                break
-            else:
-                print(user)
-        print("_________________________________________________")
+    print("_________________________________________________")
+    for user in read_file().split('\n'):
+        print(user)
+    print("_________________________________________________")
+
+def GetUser():
+    sname = input("Введите Фамилию: " + "\n") + " "
+    name = input("Введите Имя: " + "\n") + " "
+    fname = input("Введите Отчество: " + "\n") + " "
+    number = input("Введите номер: " + "\n") + " "
+    return (sname + name + fname + number)
 
 def EditBase():
-    with open('base.txt', 'a', encoding="utf-8") as file:
-        print("_________________________________________________")
-        string = input("Введите данные в формате 'Фамилия Имя Отчество номер телефона': " + "\n")
-        if not string:
-            ShowBase()
-        else:
-            file.write("\n" + string)
-        print("_________________________________________________")
-    ShowBase()
+    print("_________________________________________________")
+    write_file(GetUser())
+    print("_________________________________________________")
 
 def FindInBase():
-    with open('base.txt', 'r', encoding="utf-8") as file:
-        print("_________________________________________________")
-        element = input("Введите одну из характеристик записи для поиска: " + "\n")
-        while True:
-            el = file.readline().split()
-            for i in el:
-                if i.lower() == element.lower():
-                    print("Нашел:" + str.join(" ", el))
-            if not el:
-                break
-        print("_________________________________________________")
+    print("_________________________________________________")
+    element = input("Введите одну из характеристик записи для поиска: " + "\n")
+    for user in read_file().split("\n"):
+        for userDetail in user.split():
+            if userDetail.lower() == element.lower():
+                    print("Нашел: " +  user)
+    print("_________________________________________________")
 def Main():
     ShowBase()
     print("Если хотите добавлять данные, напишите 'add' или 'добавить'")
     print("Если хотите найти какую то запись, напишите 'search' или 'найти'")
+    print("Если хотите выйти из программы, напишите 'end' или 'закончить'")
     string = input()
     if string == 'add' or string == 'добавить':
         EditBase()
     if string == 'search' or string == 'найти':
         FindInBase()
+    if string == 'end' or string == 'закончить':
+        return 'end'
 
 while True:
-    Main()
+    if Main() == 'end':
+        break
+    else:
+        Main()
